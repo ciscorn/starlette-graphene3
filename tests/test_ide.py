bb860390cec2d1fb2fd891475c7bacd04408b0c0
Taku Fukada <naninunenor@gmail.com>
2021-05-30 00:19:32 +0900

    Fix test for IDE

--- tests/test_ide.py
+++ tests/test_ide.py
@@ -1,30 +1,49 @@
 from starlette.applications import Starlette
+from starlette.responses import HTMLResponse
 from starlette.testclient import TestClient
 
-from starlette_graphene3 import GraphQLApp
-
-
-def test_http_get_playground_enabled(schema):
-    app = Starlette()
-    app.mount("/", GraphQLApp(schema))
-    client = TestClient(app)
-
-    assert client.get("/").status_code == 200
+from starlette_graphene3 import (
+    GraphQLApp,
+    make_graphiql_handler,
+    make_playground_handler,
+)
 
 
 def test_http_get_graphiql_enabled(schema):
     app = Starlette()
-    app.mount("/", GraphQLApp(schema, IDE="graphiql"))
+    app.mount("/", GraphQLApp(schema, on_get=make_graphiql_handler()))
     client = TestClient(app)
-
     assert client.get("/").status_code == 200
+
+
+def test_http_get_playground_enabled(schema):
+    app = Starlette()
+    app.mount("/", GraphQLApp(schema, on_get=make_playground_handler()))
+    client = TestClient(app)
+    assert client.get("/").status_code == 200
+
+    app = Starlette()
+    app.mount("/", GraphQLApp(schema, playground=True))
+    client = TestClient(app)
+    assert client.get("/").status_code == 200
+
+
+def test_http_get_async_handler(schema):
+    async def handler(request):
+        return HTMLResponse("hello")
+
+    app = Starlette()
+    app.mount("/", GraphQLApp(schema, on_get=handler))
+    client = TestClient(app)
+    res = client.get("/")
+    assert res.status_code == 200
+    assert res.text == "hello"
 
 
 def test_http_get_ide_disabled(schema):
     app = Starlette()
-    app.mount("/", GraphQLApp(schema, IDE=None))
+    app.mount("/", GraphQLApp(schema, on_get=None))
     client = TestClient(app)
-
     assert client.get("/").status_code == 405
 
 
